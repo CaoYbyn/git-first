285796f6ef716070a3251f3ae49349439f478dd1
CaoYbyn <1210274965@qq.com>
2018-10-28 19:19:45 +0800

    first commit

---
+++ first-test/src/com/itheima/git/FirstTest.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class FirstTest {
+    public static void main(String[] args) {
+
+        System.out.println("first test");
+        System.out.println("first test");
+        System.out.println("first test");
+
+
+    }
+}
